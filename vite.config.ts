cf49308e0d5a3d7aba4d8b149cfbf851cc82aacf
Siedel <74981165+Saideru@users.noreply.github.com>
2026-04-25 15:26:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    publicDir: 'public', 
+    publicDir: 'public1', 
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
